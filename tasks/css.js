7762189ae47b30b54499cf112add7b6946d8870e
Chris Sheppard <github@chrissheppard.co.uk>
2015-03-25 22:45:27 +0000

    Adding normalize.css

--- tasks/css.js
+++ tasks/css.js
@@ -1,12 +1,18 @@
 var gulp = require("gulp");
 var plugins = require("gulp-load-plugins")();
+var streamqueue = require("streamqueue");
 
 
 // Compile CSS
 gulp.task("css", function () {
-    return gulp.src("./src/assets/scss/*.scss")
+    var stream = streamqueue({objectMode: true});
+    stream.queue(gulp.src("./bower_components/normalize.css/normalize.css"));
+    stream.queue(gulp.src("./src/assets/scss/*.scss").pipe(plugins.plumber()).pipe(plugins.sass()).pipe(plugins.plumber.stop()));
+
+    return stream.done()
         .pipe(plugins.plumber())
-        .pipe(plugins.sass())
+        .pipe(plugins.concat("main.css"))
+        .pipe(plugins.stripCssComments({all: true}))
         .pipe(plugins.autoprefixer({
             browsers: ["last 2 versions"],
             cascade: false
