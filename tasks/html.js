16bd3c5f214562a95360c00ea56d0f6ee5ba7793
Chris Sheppard <chris.sheppard@thebioagency.com>
2015-03-26 11:26:52 +0000

    Adding json data to jade templates

--- tasks/html.js
+++ tasks/html.js
@@ -1,11 +1,20 @@
 var gulp = require("gulp");
 var plugins = require("gulp-load-plugins")();
+var fs = require("fs");
+var path = require("path");
+var _ = require("lodash");
 
 
 // Compile jade files to HTML
 gulp.task("html", function () {
     return gulp.src("./src/templates/pages/**/*.jade")
         .pipe(plugins.plumber())
+        .pipe(plugins.data(function(file) {
+            // Extend with global data
+            var global = JSON.parse(fs.readFileSync("./src/data/global.json"));
+            var page = JSON.parse(fs.readFileSync("./src/data/" + path.basename(file.path, ".jade") + ".json"));
+            return _.extend({}, global, page);
+        }))
         .pipe(plugins.jade({
             pretty: true
         }))
