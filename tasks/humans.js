00ac9bdb44cac815137cd6296b24044dfec14616
Chris Sheppard <chris.sheppard@thebioagency.com>
2015-03-26 15:13:31 +0000

    Humans.txt

---
+++ tasks/humans.js
@@ -0,0 +1,23 @@
+var gulp = require("gulp");
+var plugins = require("gulp-load-plugins")();
+
+gulp.task("humans", function() {
+    gulp.src("./public/**/*.html")
+        .pipe(plugins.humans({
+            header: "Chris Sheppard",
+            team: {
+                "Chris Sheppard": {
+                    Twitter: "@ikiholygoat",
+                    Website: "http://www.chrissheppard.co.uk/",
+                    Country: "United Kingdom"
+                }
+            },
+            site: {
+                Standards: "HTML5, CSS3",
+                Components: "Normalize.css",
+                Software: "WebStorm"
+            },
+            note: "Built with love by Chris Sheppard.",
+            out: "./public/humans.txt"
+        }));
+});
