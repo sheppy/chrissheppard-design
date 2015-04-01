4da262c1d3615cc4b82a568a0a2b747d0ba82406
Chris Sheppard <chris.sheppard@thebioagency.com>
2015-04-01 13:02:55 +0100

    Add modernizr

---
+++ tasks/modernizr.js
@@ -0,0 +1,15 @@
+/*eslint-env node */
+
+var gulp = require("gulp");
+var plugins = require("gulp-load-plugins")();
+var config = require("./config");
+
+
+// Compile Modernizr
+gulp.task("modernizr", function () {
+    gulp.src([config.src.js, config.src.sass])
+        .pipe(plugins.plumber())
+        .pipe(plugins.modernizr())
+        .pipe(plugins.plumber.stop())
+        .pipe(gulp.dest(config.dist.jsDir));
+});
