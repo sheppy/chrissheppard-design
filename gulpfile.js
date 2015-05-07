465558214a98afa3a604b81eb6db62d5481e4405
Chris Sheppard <chris.sheppard@thebioagency.com>
2015-05-07 16:17:43 +0100

    Fixing gulp-watch with babel

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,12 @@
 
 // Enable ES6 support
 require("babel/register");
+
+// Fix babel messing up gulp-watch
+// https://github.com/babel/babel/issues/489
+Object.getPrototypeOf.toString = function () {
+    return Object.toString();
+};
+
+// Load gulp tasks
 require("require-dir")("./tasks");
-
-// Possible additions
-
-// gulp-favicons
-// gulp-bump
-// gulp-git
